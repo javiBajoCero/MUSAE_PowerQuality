8ec994b93aa6c8e7e15c56a5bfaab6face2f1151
javi <javier.munoz.saez@upc.edu>
2023-09-25 18:15:23 +0200

    put in place all printf, reviewed once more, about to send to the teacher

--- homework 1/Prog1_javi.m
+++ homework 1/Prog1_javi.m
@@ -63,7 +63,7 @@ PHIInrad=angle(fft_parc_0);
     corr_Ldesc=y_Ldesc(:,3);        % Intensidad.
 
 %% 1)Plotting waves
-subplot(4,2,1);                     %divide the window to accomodate various plots, select the first division
+subplot(3,2,1);                     %divide the window to accomodate various plots, select the first division
 yyaxis left                         %activate the right axis of the first plot
 plot(t(1:size(t)),u(1:size(t)));    %plot data, volts and time
 ylabel('[Volts]');                  %name the plot's left vertical axis
@@ -73,7 +73,7 @@ plot(t(1:size(t)),corr(1:size(t))); %plot data, current and time
 xlabel('[seconds]');                %name the plot's horizontal axis
 ylabel('[Amperes]');                %name the plot's right vertical axis
 
-subplot(4,2,2);                                 %the same all over again bu with different plot data
+subplot(3,2,2);                                 %the same all over again bu with different plot data
 yyaxis left                                    
 plot(t_Ldesc(1:size(t)),u_Ldesc(1:size(t)));    
 ylabel('[Volts]');                              
@@ -127,33 +127,47 @@ Uef_squared=Uef.*Uef;                                   % square the Uef array
 Ief_squared=Ief.*Ief;                                   % the same with Ief
 THD_Uef=100*(sqrt(sum(Uef_squared(2:end))))/Uef(1); % calculate THD, not including the fundamental component (harmonic order 1)
 THD_Ief=100*(sqrt(sum(Ief_squared(2:end))))/Ief(1); % the same with Ief
+fprintf('THDv Fmonof= %f [percent].\n',THD_Uef);
+fprintf('THDi Fmonof= %f [percent].\n',THD_Ief);
 
 Uef_squared_Ldesc=Uef_Ldesc.*Uef_Ldesc;                     %the same for Ldesc 
 Ief_squared_Ldesc=Ief_Ldesc.*Ief_Ldesc;
 THD_Uef_Ldesc=100*(sqrt(sum(Uef_squared_Ldesc(2:end))))/Uef_Ldesc(1);
 THD_Ief_Ldesc=100*(sqrt(sum(Ief_squared_Ldesc(2:end))))/Ief_Ldesc(1);
+fprintf('THDv Ldesc= %f [percent].\n',THD_Uef_Ldesc);
+fprintf('THDi Ldesc= %f [percent].\n',THD_Ief_Ldesc);
 
 %%RMS values: all signals have a Dc component =0 so we can work with previous arrays.
 Urms=sqrt(sum(Uef_squared));
 Irms=sqrt(sum(Ief_squared));
+fprintf('Urms Fmonof= %f [V].\n',Urms);
+fprintf('Irms Fmonof= %f [A].\n',Irms);
 
 Urms_Ldesc=sqrt(sum(Uef_squared_Ldesc));% the same for Ldesc
 Irms_Ldesc=sqrt(sum(Ief_squared_Ldesc));
+fprintf('Urms Ldesc= %f [V].\n',Urms_Ldesc);
+fprintf('Irms Ldesc= %f [A].\n',Irms_Ldesc);
 
 %%active power P [w]
 P=sum((Uef.*Ief).*cos(PHIInrad));
 P_Ldesc=sum((Uef_Ldesc.*Ief_Ldesc).*cos(PHIInrad_Ldesc));
+fprintf('P Fmonof= %f [W].\n',P);
+fprintf('P Ldesc= %f [W].\n',P_Ldesc);
 
 %%apparent power S = Urms+Irms [va]
 S=Urms*Irms;
 S_Ldesc=Urms_Ldesc*Irms_Ldesc;
+fprintf('S Fmonof= %f [VA].\n',S);
+fprintf('S Ldesc= %f [VA].\n',S_Ldesc);
 
 %power factor
 PF=P/S;
 PF_Ldesc=P_Ldesc/S_Ldesc;
+fprintf('PF, Power factor Fmonof= %f .\n',PF);
+fprintf('PF, Power factor Ldesc= %f .\n',PF_Ldesc);
 
 %plot results
-subplot(4,2,3);                                 %divide the window to accomodate various plots, select the third division
+subplot(3,2,3);                                 %divide the window to accomodate various plots, select the third division
 U_HDx(1)=0;                                     %override to 0 the first harmonic
 bar(U_HDx);                                     %bar plot
 title('Fmonoff Voltage Harmonic Distortions (1st harmonic forced to 0)');
@@ -162,7 +176,7 @@ ylabel('[%]');
 xticks(1:5);
 grid on;
 
-subplot(4,2,4);                                 %divide the window to accomodate various plots, select the forth division
+subplot(3,2,4);                                 %divide the window to accomodate various plots, select the forth division
 U_HDx_Ldesc(1)=0;                               %override to 0 the first harmonic
 bar(U_HDx_Ldesc);                               %bar plot
 title('Ldesc Voltage Harmonic Distortions (1st harmonic forced to 0)');
@@ -171,7 +185,7 @@ ylabel('[%]');
 xticks(1:5);
 grid on;
 
-subplot(4,2,5);                                 %divide the window to accomodate various plots, select the fifth division
+subplot(3,2,5);                                 %divide the window to accomodate various plots, select the fifth division
 Curr_HDx(1)=0;                                  %override to 0 the first harmonic        
 bar(Curr_HDx);                                  %bar plot
 title('Fmonoff Current Harmonic Distortions (1st harmonic forced to 0)');
@@ -180,7 +194,7 @@ ylabel('[%]');
 xticks(1:40);
 grid on;
 
-subplot(4,2,6);                                 %divide the window to accomodate various plots, select the sixth division
+subplot(3,2,6);                                 %divide the window to accomodate various plots, select the sixth division
 Curr_HDx_Ldesc(1)=0;                            %override to 0 the first harmonic    
 bar(Curr_HDx_Ldesc);                            %bar plot
 title('Ldesc Current Harmonic Distortions (1st harmonic forced to 0)');
